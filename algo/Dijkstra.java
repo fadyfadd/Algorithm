1e9e562c109fcfa2e8f7806f9da4ad385744a7db
fadyfadd <fady_faddoul@hotmail.com>
2024-12-22 15:23:34 +0200

    dijkstra

--- algo/Dijkstra.java
+++ algo/Dijkstra.java
@@ -44,14 +44,63 @@ public class Dijkstra {
     public List<String> path = null;
     public Long shortestDistance = -1L;
     private DijkstraQueue dijkstraQueue = null;
-    Map<String , String> previous;
-    Map<String , Long> distance;
+    Map<String, String> previous;
+    Map<String, Long> distance;
 
     public void DijkstraPath(String start, String finish, Graph graph) {
         dijkstraQueue = new DijkstraQueue();
         path = new ArrayList<>();
         previous = new HashMap<>();
         distance = new HashMap<>();
+
+        var entrySet = graph.getContent().entrySet().iterator();
+
+        while (entrySet.hasNext()) {
+            var key = entrySet.next().getKey();
+
+            if (key == start) {
+                distance.put(key, 0L);
+                dijkstraQueue.put(new PriorityItem(key, 0L));
+            } else {
+                distance.put(key, Long.MAX_VALUE);
+                dijkstraQueue.put(new PriorityItem(key, Long.MAX_VALUE));
+            }
+
+            previous.put(key, null);
+        }
+
+        while (dijkstraQueue.contains()) {
+
+            var currentNode = dijkstraQueue.getPriorityNode();
+
+            if (currentNode.item == finish) {
+                while (previous.get(currentNode.item) != null) {
+                    path.add(currentNode.item);
+                    currentNode.item = previous.get(currentNode.item);
+                }
+                break;
+            }
+
+            if (currentNode.weight != Long.MAX_VALUE) {
+
+                var children = graph.getContent().get(currentNode.item);
+                for (int i = 0; i <= children.size() - 1; i++) {
+
+                    var newWeight = children.get(i).weight + currentNode.weight;
+                    var currentWeight = distance.get(children.get(i));
+
+                    if (newWeight < currentWeight) {
+                        distance.put(children.get(i).data, newWeight);
+                        previous.put(children.get(i).data, currentNode.item);
+                        dijkstraQueue.put(new PriorityItem(children.get(i).data, newWeight));
+                    }
+                }
+            }
+        }
+
+        path.add(start);
+        path = path.reversed();
+        shortestDistance = distance.get(finish);
     }
 
     public static void main(String... args) {
@@ -73,6 +122,11 @@ public class Dijkstra {
         graph.addEdge("D", new Node("E", 3L));
         graph.addEdge("D", new Node("F", 1L));
         graph.addEdge("E", new Node("F", 1L));
+
+        var inst = new Dijkstra();
+        inst.DijkstraPath("A" , "E" , graph);
+        inst.path.stream().forEach((a)->System.out.print(a + " "));
+        System.out.println(inst.shortestDistance);
 
     }
 
